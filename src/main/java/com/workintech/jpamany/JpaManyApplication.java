849e0ad597885dceb19739351389fa34dffe8ec8
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-09-07 13:53:02 +0300

    S18-D4 All task done

--- src/main/java/com/workintech/jpamany/JpaManyApplication.java
+++ src/main/java/com/workintech/jpamany/JpaManyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaManyApplication {
+public class   JpaManyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaManyApplication.class, args);
